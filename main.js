1b3c6cb8356ce0ce0b72472381afe26c8c883bf5
Aramuad <samcballard@gmail.com>
2018-08-20 17:30:38 -0600

    add rock paper scissors JS  attempt

---
+++ main.js
@@ -0,0 +1,8 @@
+var rockBtn = document.getElementById(rock)
+var paperBtn = document.getElementById(paper)
+var scissorsBtn = document.getElementById(scissors)
+
+
+choooseRock = function(choice) {
+  
+}
